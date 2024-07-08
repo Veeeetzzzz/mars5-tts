9326bd91f52267b9926666c61666dd3a728b6ee4
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-07-08 21:36:22 +0100

    Update tts.py

--- tts.py
+++ tts.py
@@ -7,7 +7,6 @@ import time
 import sys
 
 # Experimental - don't uncomment yet
-# At the beginning of your script, add:
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # print(f"Using device: {device}")
 
